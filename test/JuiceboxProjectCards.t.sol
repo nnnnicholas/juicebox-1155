3555447ef9f38f8691947c4ff947ba75e9d0bf75
nicholas <nnnnicholas@users.noreply.github.com>
2023-04-05 14:30:39 -0400

    test improvements

--- test/JuiceboxProjectCards.t.sol
+++ test/JuiceboxProjectCards.t.sol
@@ -38,6 +38,9 @@ contract JuiceboxProjectCardsTest is Test, ERC1155Receiver {
     uint256 public constant PRICE_IN_WEI = 0.01 ether;
     string public constant CONTRACT_URI = "xyz";
 
+    // Get count of deployed JB Projects 
+    uint jbProjectsCount = JBProjects(JBPROJECTS).count();
+    
     // Config
     Config public config =
         Config(JBPROJECTS, REVENUE_RECIPIENT, PRICE_IN_WEI, CONTRACT_URI);
@@ -52,7 +55,7 @@ contract JuiceboxProjectCardsTest is Test, ERC1155Receiver {
     // Test that you can mint an NFT
     function testMintOne(uint x, uint y) public {
         x = bound(x, PRICE_IN_WEI, 100 ether); // Assume that x is is greater than the price (0.01 ETH) and less than 100 ETH
-        y = bound(y, 1, JBProjects(JBPROJECTS).count()); // Assume that the project ID is greater than 0 and less than 400
+        y = bound(y, 1, jbProjectsCount); // Assume that the project ID is greater than 0 and less than the total supply of JBProjects
         vm.deal(address(420), x); // Send 420 x ETH
         vm.startPrank(address(420)); // Set the prank address to 420
         uint balanceBefore = address(juiceboxProjectCards).balance; // Get balance of the NFT contract before minting
@@ -75,7 +78,7 @@ contract JuiceboxProjectCardsTest is Test, ERC1155Receiver {
     // Test that user can mintMany
     function testMintMany(uint x, uint y) public {
         x = bound(x, 1, 100); // Assume that the number of NFTs to mint is 0 < x < 100
-        y = bound(y, 1, 400); // Assume that the project ID to mint is less than 400
+        y = bound(y, 1, jbProjectsCount); // Assume that the project ID to mint is less than total supply of JBProjects
         vm.deal(address(420), 10 ether); // Send 420 x ETH
         vm.startPrank(address(420)); // Set the prank address to 420
         uint balanceBefore = address(juiceboxProjectCards).balance; // Get balance of the NFT contract before minting
