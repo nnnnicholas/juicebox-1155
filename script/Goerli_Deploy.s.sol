0598afca76d2b1d3fe759b8a54bdc97bdd062370
nicholas <nnnnicholas@users.noreply.github.com>
2023-06-07 14:46:39 -0400

    fixed test file names

--- script/Goerli_Deploy.s.sol
+++ script/Goerli_Deploy.s.sol
@@ -2,13 +2,14 @@
 pragma solidity ^0.8.17;
 
 import "forge-std/Script.sol";
-import {JuiceboxProjectCards} from "src/JuiceboxProjectCards.sol";
+import {JuiceboxCards} from "src/JuiceboxCards.sol";
 import {IJBProjectPayer} from "@jbx-protocol/juice-contracts-v3/contracts/JBETHERC20ProjectPayer.sol";
 import {IJBProjects} from "@jbx-protocol/juice-contracts-v3/contracts/interfaces/IJBProjects.sol";
 import {Config} from "src/Structs/Config.sol";
 
 contract DeployScript is Script {
     // Goerli constructor args
+    address public directory = 0x8E05bcD2812E1449f0EC3aE24E2C395F533d9A99;
     address public projects = 0x21263a042aFE4bAE34F08Bb318056C181bD96D3b;
     address payable public projectPayer =
         payable(0x0e44297757856fc9236A130D11B0547d42Ce27d8);
@@ -17,13 +18,13 @@ contract DeployScript is Script {
 
     // Config
     Config public config =
-        Config(projects, projectPayer, priceInWei, contractUri);
+        Config(projects, projectPayer, priceInWei, contractUri, directory);
 
     function run() public {
         uint256 deployerPrivateKey = vm.envUint("GOERLI_PRIVATE_KEY");
         // Deploy Juicebox1155
         vm.startBroadcast(deployerPrivateKey);
-        JuiceboxProjectCards juiceboxProjectCards = new JuiceboxProjectCards(config);
+        JuiceboxCards juiceboxCards = new JuiceboxCards(config);
         vm.stopBroadcast();
     }
 }
--- script/Mainnet_Deploy.s.sol
+++ script/Mainnet_Deploy.s.sol
@@ -2,30 +2,30 @@
 pragma solidity ^0.8.17;
 
 import "forge-std/Script.sol";
-import {JuiceboxProjectCards} from "src/JuiceboxProjectCards.sol";
+import {JuiceboxCards} from "src/JuiceboxCards.sol";
 import {IJBProjectPayer} from "@jbx-protocol/juice-contracts-v3/contracts/JBETHERC20ProjectPayer.sol";
 import {IJBProjects} from "@jbx-protocol/juice-contracts-v3/contracts/interfaces/IJBProjects.sol";
 import {Config} from "src/Structs/Config.sol";
 
 contract DeployScript is Script {
-    // Mainnet constructor args
+    address public directory =
+        address(0x65572FB928b46f9aDB7cfe5A4c41226F636161ea);
     address public projects = 0xD8B4359143eda5B2d763E127Ed27c77addBc47d3;
     address payable public projectPayer =
         payable(0xD37b2FE8748f4795a465c9B851ce8066426A427F);
     uint256 public priceInWei = 0.01 ether;
-    string public contractUri = "ipfs://QmYGEhsCwMdmcZSmhEvpJXvSM4Jgvj4WyRgdwH53ZHtUar";
+    string public contractUri =
+        "ipfs://QmYGEhsCwMdmcZSmhEvpJXvSM4Jgvj4WyRgdwH53ZHtUar";
 
     // Config
     Config public config =
-        Config(projects, projectPayer, priceInWei, contractUri);
+        Config(projects, projectPayer, priceInWei, contractUri, directory);
 
     function run() public {
         uint256 deployerPrivateKey = vm.envUint("MAINNET_PRIVATE_KEY");
         // Deploy Juicebox1155
         vm.startBroadcast(deployerPrivateKey);
-        JuiceboxProjectCards juiceboxProjectCards = new JuiceboxProjectCards(
-            config
-        );
+        JuiceboxCards juiceboxCards = new JuiceboxCards(config);
         vm.stopBroadcast();
     }
 }
