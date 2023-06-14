046e58df01d9949958850e08120cea49b41b116f
nicholas <nnnnicholas@users.noreply.github.com>
2023-06-14 14:47:55 -0400

    updated name of config var to tip

--- src/Structs/Config.sol
+++ src/Structs/Config.sol
@@ -3,7 +3,7 @@ pragma solidity >=0.8.0;
 
 struct Config {
     address jbProjects; // 160 bits  // The JBProjects contract
-    uint16 feeProject;  // 16 bits   // The project ID of the metadata project
+    uint16 tipProject;  // 16 bits   // The project ID of the metadata project
     uint64 price;       // 64 bits   // The price of the NFT in wei
     address directory;  // 160 bits  // The directory contract
     string contractUri; // 256+ bits // The URI of the contract metadata
